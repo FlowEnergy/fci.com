03f5bd74526193f5c10563b41e936b16797a3fdb
Paul Kearney <paul@gravitypad.com>
2016-01-14 20:34:26 -0800

    Copy gulpfile from flowenergy; add readme

---
+++ README.md
@@ -0,0 +1,18 @@
+npm install --global gulp
+
+sudo npm install gulp-sourcemaps gulp-jade gulp-sourcemaps gulp-less-sourcemap path gulp-watch gulp-minify-css gulp-imagemin gulp-less-import gulp-connect gulp-concat gulp-uglify gulp-awspublish
+
+// if warning delete - gulp-watch
+
+
+// Сommands
+
+	gulp watch - start watch
+	gulp compress-img - compress all images
+    gulp jade - rebuild html files
+    gulp - start http://localhost:8080
+    gulp publish - publish to aws
+
+minifyCSS - default off (delete comment but It will not work map.less)
+
+also, for reading, http://webapplog.com/jade/
--- gulpfile.js
+++ gulpfile.js
@@ -1,3 +1,4 @@
+
 var gulp = require('gulp'),
 	jade = require('gulp-jade'),
 	lessImport = require('gulp-less-import'),
@@ -7,7 +8,10 @@ var gulp = require('gulp'),
 	watch = require('gulp-watch'),
 //	minifyCSS = require('gulp-minify-css'),
  	connect = require('gulp-connect'),
-	imagemin = require('gulp-imagemin');
+	imagemin = require('gulp-imagemin'),
+	concat = require('gulp-concat'),
+	uglify = require('gulp-uglify'),
+	awspublish = require('gulp-awspublish');
 
 gulp.task('connect', function() {
   connect.server({
@@ -18,20 +22,26 @@ gulp.task('connect', function() {
 
 // Compress Task
  gulp.task('compress-img', function() {
-	gulp.src('build/img/*')
+	gulp.src('build/img/**')
 		.pipe(imagemin({
 			progressive: true
 		}))
-	.pipe(gulp.dest('build/img/'));
+	.pipe(gulp.dest('src/img/'));
 });
 
 gulp.task('jade', function() {
-	gulp.src('build/jade/page/*.jade')
+	gulp.src('build/jade/*.jade')
 		.pipe(jade({
 			locals: {}
 		}))
-		.pipe(gulp.dest('src/html/'))
+		.pipe(gulp.dest('src/'))
 		.pipe(connect.reload());
+});
+
+gulp.task('scripts', function() {
+	gulp.src('build/js/*.js')
+		.pipe(uglify())
+		.pipe(gulp.dest('src/js'))
 });
 
 gulp.task('less', function() {
@@ -40,7 +50,7 @@ gulp.task('less', function() {
 		.pipe(lessImport('./build/less/style.less'))
 		.pipe(less({
 			sourceMap: {
-				sourceMapRootpath: './build/less' // Optional absolute or relative path to your LESS files 
+				sourceMapRootpath: './build/less' // Optional absolute or relative path to your LESS files
 			}
 		}))
 		.on('error', function(err){ console.log(err.message); })
@@ -48,14 +58,27 @@ gulp.task('less', function() {
 		// .pipe(minifyCSS())
 		.pipe(gulp.dest('./src/css/'))
 		.pipe(connect.reload());
+	gulp.src('./build/css/*.css')
+		.pipe(gulp.dest('./src/css'));
 });
 
+gulp.task('publish', function() {
+	var publisher = awspublish.create({
+		params: {
+			Bucket: 'flowenergy.com'
+		}
+	});
+	gulp.src('src/**/*')
+		.pipe(publisher.publish())
+		.pipe(publisher.cache())
+		.pipe(awspublish.reporter())
+});
 
 gulp.task('watch', function(){
 	gulp.watch('build/jade/**/*.jade', ['jade']);
-	gulp.watch('build/js/**/*.js', ['jade']);
 	gulp.watch('build/**/*.less', ['less']);
+	gulp.watch('build/js/*.js', ['scripts']);
 });
- 
- 
-gulp.task('default', ['watch','connect', 'compress-img']);
+
+gulp.task('build', ['scripts', 'jade', 'compress-img', 'less']);
+gulp.task('default', ['scripts', 'jade', 'compress-img', 'less', 'watch','connect']);
